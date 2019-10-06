258c33ac37a198b8f8324b548ce3d3a402c04e68
Leonard Thiele <leonard.thiele@eon.com>
2019-10-06 15:27:41 +0200

    improve console service

--- Angular/consolehelper/console.service.ts
+++ Angular/consolehelper/console.service.ts
@@ -5,11 +5,13 @@ import { Injectable, Inject } from '@angular/core';
 export class ConsoleHelperService {
     private helpTextList: { [fnName: string]: string };
     private customNamespace: { [fnName: string]: (...args: any[]) => any };
+    private namespace: string;
 
     constructor(@Inject('window') window: Window) {
         this.helpTextList = {};
         this.customNamespace = {};
-        (<any>window).custom = this.customNamespace;
+        this.namespace = 'custom';
+        window[this.namespace] = this.customNamespace;
         this.registerFunction('help', () => this.help(), 'Show this help text');
     }
 
